3631d229b1ae001fd6acaa677ac48957342b02d9
Aakaive <wonseok031@gmail.com>
2024-08-13 15:01:23 +0900

    create project

---
+++ src/main/java/com/sparta/springwork01/SpringWork01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springwork01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWork01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWork01Application.class, args);
+	}
+
+}
